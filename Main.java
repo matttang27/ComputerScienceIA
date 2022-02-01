c8e3eb424659925d03beccb13ccbbf4e718d8769
Matthew Tang <matttang27@gmail.com>
2022-02-01 00:22:32 -0500

    Small Thing

--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
 import java.time.LocalDateTime;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 class Main {
     public static void main(String args[]) {
-        System.out.println("HI");
-        Manager manager = new Manager();
-        manager.tasker.blankTask();
-        System.out.println(manager.tasker);
         
+
+        
+        Scanner input = new Scanner(System.in);
+        Manager manager = new Manager();
+        TaskManager taskManager = manager.getTasker();
+        System.out.println("Commands:\n\n1) New Task\n\n2) View all tasks\n\n");
+        while (true) {
+            switch (input.nextLine()) {
+                case "1":
+                    System.out.println("Enter Task Name:");
+                    String name = input.nextLine();
+                    
+                    
+            }
+        }
     }
 }
